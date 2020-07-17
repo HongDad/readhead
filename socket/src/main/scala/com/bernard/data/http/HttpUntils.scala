fa34fe42cb736bf95ca5104a4d4ff78c06d4faf9
weidaihong <15110225531@163.com>
2020-07-17 19:06:20 +0800

    http请求接口提交

--- socket/src/main/scala/com/bernard/data/http/HttpUntils.scala
+++ socket/src/main/scala/com/bernard/data/http/HttpUntils.scala
@@ -5,14 +5,29 @@ import scalaj.http.Http
 object HttpUntils {
   def main(args: Array[String]): Unit = {
     val url = "http://openapi.yoyi.com.cn/openapi/v1/campaign/listAllCampaign"
-    val json = ""
-    val response = Http(url)
-      .header("token","13814O87nM48014339O9T03x735zN0gOLP08")
-      .header("Content-Type","application/json; charset=UTF-8")
-      .header("Accept","application/json; charset=UTF-8")
-      .asString
+    val body = "{\"userName\":\"adplus@yoyi.com.cn\",\"passWord\":\"Z4IaFHLHMPUQ2018\",\"systemId\":\"1\"}"
+    val head = Map(
+    //  "token" -> "13814O87nM48014339O9T03x735zN0gOLP08",
+      "Content-Type" -> "application/json; charset=UTF-8",
+      "Accept" -> "application/json; charset=UTF-8"
+    )
+    println(postHead(url, head,body))
+  }
 
+  def postHead(url: String, header: Map[String, String], body: String) = {
+    val request = Http(url)
+    for ((key, value) <- header) {
+      request.header(key, value)
+    }
+     request.postData(body).asString
+  }
 
-    println(response.body)
+  //从接口拿取数据,通过get的方式
+  def get(url: String, header: Map[String, String]) = {
+    val request = Http(url)
+    for ((key, value) <- header) {
+      request.header(key, value)
+    }
+    request.asString
   }
 }
